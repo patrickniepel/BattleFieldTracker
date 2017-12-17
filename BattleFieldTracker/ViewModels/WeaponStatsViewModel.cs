7b42f1bd19967e451c86430c55a88b422b6c5e83
Patrick Niepel <patrickniepel@web.de>
2017-12-17 22:08:21 +0100

    - Neue Font Größe hinzugefügt - Layout für General und Details soweit fertig gemacht

--- BattleFieldTracker/ViewModels/WeaponStatsViewModel.cs
+++ BattleFieldTracker/ViewModels/WeaponStatsViewModel.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using BattleFieldTracker.Annotations;
+using BattleFieldTracker.Commands;
 using BattleFieldTracker.Download;
 using BattleFieldTracker.DownloadModels;
 
@@ -6,19 +11,44 @@ namespace BattleFieldTracker.ViewModels
 {
     public class WeaponStatsViewModel : BaseViewModel
     {
-        private List<ResultWeaponStats> _weapons;
+        private List<ResultWeaponStats> _allWeapons;
+        //private ObservableCollection<ResultWeaponStats> _weapons;
+        //private string _filterText;
         private bool _downloadFinished;
 
-        public List<ResultWeaponStats> Weapons
+        //public DelegateCommand ClearFilterCommand { get; set; }
+
+        public List<ResultWeaponStats> AllWeapons
         {
-            get => _weapons;
-            set => Set(ref _weapons, value);
+            [UsedImplicitly] get => _allWeapons;
+            set => Set(ref _allWeapons, value);
         }
+
+//        public ObservableCollection<ResultWeaponStats> Weapons
+//        {
+//            [UsedImplicitly] get => _weapons;
+//            set => Set(ref _weapons, value);
+//        }
+//
+//        public string FilterText
+//        {
+//            get => _filterText;
+//            set
+//            {
+//                Set(ref _filterText, value);
+//                Filter();
+//            }
+//        }
 
         public bool DownloadFinished
         {
-            get => _downloadFinished;
+            [UsedImplicitly] get => _downloadFinished;
             set => Set(ref _downloadFinished, value);
+        }
+
+        public WeaponStatsViewModel()
+        {
+            //ClearFilterCommand = new DelegateCommand(ClearFilterCommandExecute);
         }
 
         public void DownloadWeaponStats(string playerName)
@@ -36,7 +66,26 @@ namespace BattleFieldTracker.ViewModels
                 return;
             }
             List<ResultWeaponStats> weapons = root.Result;
-            Weapons = weapons;
+            AllWeapons = weapons;
         }
+//
+//        private void Filter()
+//        {
+//            Weapons = string.IsNullOrWhiteSpace(FilterText)
+//                ? new ObservableCollection<ResultWeaponStats>(_allWeapons)
+//                : new ObservableCollection<ResultWeaponStats>(_allWeapons.Where(c =>
+//                    c.Name?.IndexOf(FilterText, StringComparison.CurrentCultureIgnoreCase) >= 0));
+//
+//        }
+//
+//        private void ClearFilterCommandExecute(object obj)
+//        {
+//            FilterText = "";
+//        }
+//
+//        public int CompareTo(ResultWeaponStats other)
+//        {
+//            return 
+//        }
     }
 }
